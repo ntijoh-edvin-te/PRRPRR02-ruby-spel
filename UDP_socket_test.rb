603ffd5728715253261acb328746134c0785095d
Edvin Tengvall <edvin.tengvall@elev.ga.ntig.se>
2024-10-01 21:55:23 +0200

    Update UDP_socket_test.rb

--- UDP_socket_test.rb
+++ UDP_socket_test.rb
@@ -1,16 +1,46 @@
 require 'socket'
+require 'gosu'
 
-server = Socket.new(Socket::AF_INET, Socket::SOCK_DGRAM)
-server.bind(Socket.sockaddr_in(2000,"127.0.0.1"))
+# Serializing packet UDP
 
-Thread.new do
-    loop do
-        client = Socket.new(Socket::AF_INET, Socket::SOCK_DGRAM)
-        client.send("ok",0,Socket.sockaddr_in(2000,"127.0.0.1"))
-    end 
+def pack_udp()
+
 end
 
-loop do
-    data, addr_inf = server.recvfrom(1024)
-    p data
+# Deserializing packet UDP
+
+def unpack_udp()
+
 end
+
+class Main < Gosu::Window
+    def initialize()
+        super(1920/2,1080/2)
+        @client_socket = Socket.new(Socket::AF_INET, Socket::SOCK_DGRAM) 
+    end
+
+    def update
+        @client_socket.send("W",0,Socket.sockaddr_in(2000,"127.0.0.1")) if self.button_down?(Gosu::KbW)
+    end
+end
+
+class Server
+    def initialize()
+        @server_socket = Socket.new(Socket::AF_INET, Socket::SOCK_DGRAM)
+        @server_socket.bind(Socket.sockaddr_in(2000,"127.0.0.1"))
+        @clients = []
+        start
+    end
+
+    def start()
+        Thread.new do # UDP packet handling
+            loop do
+                p @server_socket.recvfrom(1476)
+            end
+        end
+    end
+end
+
+Server.new
+Main.new.show
+
