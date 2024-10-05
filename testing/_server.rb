255de16f826806a3bcddcf1189f93591bb8bfc90
Edvin Tengvall <edvin.tengvall@elev.ga.ntig.se>
2024-10-05 21:17:41 +0200

    udp/tcp test 2

---
+++ testing/_server.rb
@@ -0,0 +1,50 @@
+require 'socket'
+require 'gosu'
+require 'securerandom'
+
+include Socket::Constants 
+
+class Server
+    def initialize()
+        # Session variables
+        @server_session_id = SecureRandom.uuid
+        puts "Server starting with session id: #{@server_session_id}"
+
+        @clients = []
+        @players = []
+        
+        # Socket constructors
+        tcp_addrinfo = Addrinfo.tcp("0.0.0.0",0)
+        udp_addrinfo = Addrinfo.udp("0.0.0.0",0)
+
+        # Handling of threads
+        @mutex = Thread::Mutex.new()
+
+        tcp_thread = Thread.new {handle_tcp_endpoint(tcp_addrinfo)}
+        udp_thread = Thread.new {handle_udp_endpoint(udp_addrinfo)}
+        [tcp_thread,udp_thread].each(&:join)
+    end
+
+    def handle_tcp_endpoint(addrinfo) # Handling of TCP streams
+        tcp_socket = Socket.new(AF_INET, SOCK_STREAM)
+        tcp_socket.bind(sockaddr) 
+        tcp_socket.listen(10)
+
+        loop do
+            @mutex.synchronize{
+            }
+        end
+    end
+
+    def handle_udp_endpoint() # Handling of UDP datagrams
+        udp_socket = Socket.new(AF_INET, SOCK_DGRAM)
+        udp_socket.bind(@udp_addr)
+        
+        loop do
+            @mutex.synchronize{
+            }
+        end
+    end
+end
+
+Server.new
