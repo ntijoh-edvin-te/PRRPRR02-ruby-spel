1489d2963245cebffa183de748ae5ac7ae123cd5
Edvin Tengvall <edvin.tengvall@elev.ga.ntig.se>
2024-10-06 10:55:48 +0200

    Update _server.rb

--- _test/_server.rb
+++ _test/_server.rb
@@ -8,17 +8,17 @@ class Server
     def initialize()
         # Session variables
         @server_session_id = SecureRandom.uuid
-        puts "Started server with session id: #{@server_session_id}"
+        puts "Server started with session id: #{@server_session_id}"
 
         @clients = []
         @players = []
         
         # Socket init
-        tcp_addrinfo = Addrinfo.tcp("0.0.0.0",0)
-        udp_addrinfo = Addrinfo.udp("0.0.0.0",0)
+        tcp_addrinfo = Addrinfo.tcp("127.0.0.1",5000)
+        udp_addrinfo = Addrinfo.udp("127.0.0.1",0)
         
         # Handling of threads
-        @mutex = Thread::Mutex.new()
+        @mutex_clients = Thread::Mutex.new()
 
         tcp_thread = Thread.new {handle_tcp_endpoint(tcp_addrinfo)}
         udp_thread = Thread.new {handle_udp_endpoint(udp_addrinfo)}
@@ -32,16 +32,19 @@ class Server
         
         loop do
             begin
-                client_socket, _ = tcp_socket.accept
-                Thread.new{
-                    puts client_socket
-                }
+                client_socket, client_addrinfo = tcp_socket.accept
+                Thread.new(client_socket,client_addrinfo) do |client,addrinfo|
+                    @mutex_clients.synchronize{
+                        @clients << Socket.unpack_sockaddr_in(addrinfo)
+                    }
+                    loop do
+                        
+                    end
+                end
             rescue => e
-                puts "An error occured when attempting a TCP 3-Way Handshake: #{e}"
+                puts "(Server) Errored when attempting TCP 3-way handshake: #{e}"
                 retry
             end
-            @mutex.synchronize{
-            }
         end
     end
 
@@ -50,10 +53,28 @@ class Server
         udp_socket.bind(addrinfo)
         
         loop do
-            @mutex.synchronize{
-            }
         end
     end
 end
 
-Server.new
+class Client 
+    def initialize()
+        @tcp_socket = Socket.new(AF_INET, SOCK_STREAM)
+        test
+    end
+
+    def test
+        begin
+            @tcp_socket.connect(Addrinfo.tcp("127.0.0.1",5000))
+        rescue => e
+            puts "(Client) Errored when attempting TCP 3-way handshake #{e}"
+            retry
+        end
+    end
+end
+
+Thread.new{Server.new}
+
+Client.new
+sleep()
+
